85edcfaad885548a456d44cdfcc7e1e711e3fb7b
Toby Larone <10854475+TobyLarone85@users.noreply.github.com>
2019-08-05 15:41:42 +0100

    [discord] Add JDA + disparse dependencies for discord integration

--- src/main/java/com/jvmrally/lambda/App.java
+++ src/main/java/com/jvmrally/lambda/App.java
@@ -1,11 +1,20 @@
 package com.jvmrally.lambda;
 
+import javax.security.auth.login.LoginException;
+import disparse.discord.Dispatcher;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 /**
- * Hello world!
+ * App Entry
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final String TOKEN = "LAMBDA_TOKEN";
+    private static final String PREFIX = "!";
+
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDA jda = Dispatcher.init(new JDABuilder(System.getenv(TOKEN)), PREFIX).build();
+        jda.awaitReady();
     }
 }
