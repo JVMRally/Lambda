84cdb5f1a492a0eccb00e7f103da1fb4aa939936
Toby Larone <10854475+TobyLarone85@users.noreply.github.com>
2019-08-16 13:42:49 +0100

    [moderation] Add unmute command. Fix mute command for already muted users

---
+++ src/main/java/com/jvmrally/lambda/command/moderation/UnMute.java
@@ -0,0 +1,33 @@
+package com.jvmrally.lambda.command.moderation;
+
+import static com.jvmrally.lambda.db.tables.Mute.MUTE;
+import java.util.List;
+import java.util.Optional;
+import com.jvmrally.lambda.Util;
+import org.jooq.DSLContext;
+import disparse.parser.reflection.CommandHandler;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+/**
+ * UnMute
+ */
+public class UnMute {
+
+    @CommandHandler(commandName = "unmute")
+    public static void mute(DSLContext dsl, MessageReceivedEvent e) {
+        List<Member> members = e.getMessage().getMentionedMembers();
+        if (members.isEmpty()) {
+            e.getChannel().sendMessage("Must mention at least one user").queue();
+            return;
+        }
+        Optional<Role> role = Util.getRole(e.getGuild(), "muted");
+        role.ifPresentOrElse(r -> {
+            for (Member member : members) {
+                e.getGuild().removeRoleFromMember(member, r);
+                dsl.deleteFrom(MUTE).where(MUTE.USERID.eq(member.getIdLong())).execute();
+            }
+        }, () -> e.getChannel().sendMessage("Role does not exist").queue());
+    }
+}
