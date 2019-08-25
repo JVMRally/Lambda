60e3c05a6569f3701bad12e770c91bc7b014a8e6
Toby Larone <10854475+TobyLarone85@users.noreply.github.com>
2019-08-25 12:41:03 +0100

    [commands.misc] add about command. Update Messenger to accept embed

--- src/main/java/com/jvmrally/lambda/utility/messaging/BuildMessage.java
+++ src/main/java/com/jvmrally/lambda/utility/messaging/BuildMessage.java
@@ -1,9 +1,13 @@
 package com.jvmrally.lambda.utility.messaging;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+
 /**
  * BuildMesage
  */
 public interface BuildMessage {
 
     Messenger message(String message);
+
+    Messenger message(EmbedBuilder embed);
 }
