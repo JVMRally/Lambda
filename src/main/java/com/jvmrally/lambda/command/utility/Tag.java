41a3d8dc1acaeb6ed8b71d9fc45266f3c59febc0
Toby Larone <10854475+Toby-Larone@users.noreply.github.com>
2019-10-31 18:08:40 +0000

    [commands.tag] refactor tag command

--- src/main/java/com/jvmrally/lambda/command/utility/Tag.java
+++ src/main/java/com/jvmrally/lambda/command/utility/Tag.java
@@ -8,7 +8,6 @@ import com.jvmrally.lambda.command.entites.TagRequest;
 import com.jvmrally.lambda.db.enums.AuditAction;
 import com.jvmrally.lambda.db.tables.pojos.Tags;
 import com.jvmrally.lambda.injectable.Auditor;
-import com.jvmrally.lambda.utility.Util;
 import com.jvmrally.lambda.utility.messaging.Messenger;
 import org.jooq.DSLContext;
 import disparse.parser.reflection.CommandHandler;
@@ -21,20 +20,7 @@ public class Tag {
 
     @CommandHandler(commandName = "tag",
             description = "Create, edit, or display useful messages. For creating and editing, place all message content between two `\"`")
-    public static void execute(Auditor auditor, DSLContext dsl, TagRequest req,
-            MessageReceivedEvent e) {
-        if (req.isManipulatingTag()) {
-            if (!Util.hasRole(e.getMember(), "admin")) {
-                Messenger.send(e.getChannel(), "You don't have permission to do that");
-                return;
-            }
-            manipulateTag(auditor, dsl, req, e);
-            return;
-        }
-        if (req.getList()) {
-            listTags(dsl, e);
-            return;
-        }
+    public static void displayTag(DSLContext dsl, TagRequest req, MessageReceivedEvent e) {
         if (!req.getName().isEmpty()) {
             dsl.selectFrom(TAGS).where(TAGS.TAGNAME.eq(req.getName())).fetchOptionalInto(Tags.class)
                     .ifPresentOrElse(tag -> Messenger.send(e.getChannel(), tag.getContent()),
@@ -42,56 +28,66 @@ public class Tag {
         }
     }
 
-    private static void manipulateTag(Auditor auditor, DSLContext dsl, TagRequest req,
-            MessageReceivedEvent e) {
-        if (req.getCreate()) {
-            createTag(auditor, dsl, req, e);
-            return;
-        }
-        if (req.getEdit()) {
-            editTag(auditor, dsl, req, e);
-            return;
-        }
-        if (req.getDelete()) {
-            deleteTag(auditor, dsl, req, e);
-            return;
-        }
+    @CommandHandler(commandName = "tag.list", description = "List tags")
+    public static void create(DSLContext dsl, MessageReceivedEvent e) {
+        listTags(dsl, e);
     }
 
-    private static void createTag(Auditor auditor, DSLContext dsl, TagRequest req,
+    @CommandHandler(commandName = "tag.admin.create",
+            description = "Create a tag. Place all message content between two `\"`",
+            roles = "admin")
+    public static void create(Auditor auditor, DSLContext dsl, TagRequest req,
             MessageReceivedEvent e) {
         if (isContentEmpty(req, e)) {
             return;
         }
-        findTag(dsl, req.getName()).ifPresentOrElse(tag -> Messenger.send(e.getChannel(),
-                "Tag `" + tag.getTagname() + "` already exists."), () -> {
-                    dsl.insertInto(TAGS).columns(TAGS.TAGNAME, TAGS.CONTENT, TAGS.UPDATED_AT)
-                            .values(req.getName(), req.getContent(), OffsetDateTime.now())
-                            .execute();
-                    auditor.log(AuditAction.CREATED_TAG, e.getAuthor().getIdLong());
-                });
+        findTag(dsl, req.getName()).ifPresentOrElse(
+                tag -> Messenger.send(e.getChannel(),
+                        "Tag `" + tag.getTagname() + "` already exists."),
+                () -> createTag(auditor, dsl, req, e));
+    }
+
+    @CommandHandler(commandName = "tag.admin.edit",
+            description = "Edit a tag. Place all message content between two `\"`", roles = "admin")
+    public static void edit(Auditor auditor, DSLContext dsl, TagRequest req,
+            MessageReceivedEvent e) {
+        if (isContentEmpty(req, e)) {
+            return;
+        }
+        findTag(dsl, req.getName()).ifPresentOrElse(tag -> editTag(auditor, dsl, req, e),
+                () -> Messenger.send(e.getChannel(), "Tag does not exist."));
+    }
+
+    @CommandHandler(commandName = "tag.admin.delete", description = "Delete a tag", roles = "admin")
+    public static void delete(Auditor auditor, DSLContext dsl, TagRequest req,
+            MessageReceivedEvent e) {
+        if (isContentEmpty(req, e)) {
+            return;
+        }
+        findTag(dsl, req.getName()).ifPresentOrElse(tag -> deleteTag(auditor, dsl, tag, e),
+                () -> Messenger.send(e.getChannel(), "Tag does not exist."));
+    }
+
+    private static void createTag(Auditor auditor, DSLContext dsl, TagRequest req,
+            MessageReceivedEvent e) {
+        dsl.insertInto(TAGS).columns(TAGS.TAGNAME, TAGS.CONTENT, TAGS.UPDATED_AT)
+                .values(req.getName(), req.getContent(), OffsetDateTime.now()).execute();
+        auditor.log(AuditAction.CREATED_TAG, e.getAuthor().getIdLong());
         Messenger.send(e.getChannel(), "Tag created!");
     }
 
     private static void editTag(Auditor auditor, DSLContext dsl, TagRequest req,
             MessageReceivedEvent e) {
-        if (isContentEmpty(req, e)) {
-            return;
-        }
-        findTag(dsl, req.getName()).ifPresentOrElse(tag -> {
-            dsl.update(TAGS).set(TAGS.CONTENT, req.getContent()).set(TAGS.UPDATED_AT,
-                    OffsetDateTime.now());
-            auditor.log(AuditAction.EDITED_TAG, e.getAuthor().getIdLong());
-        }, () -> Messenger.send(e.getChannel(), "Tag does not exist."));
+        dsl.update(TAGS).set(TAGS.CONTENT, req.getContent()).set(TAGS.UPDATED_AT,
+                OffsetDateTime.now());
+        auditor.log(AuditAction.EDITED_TAG, e.getAuthor().getIdLong());
     }
 
-    private static void deleteTag(Auditor auditor, DSLContext dsl, TagRequest req,
+    private static void deleteTag(Auditor auditor, DSLContext dsl, Tags tag,
             MessageReceivedEvent e) {
-        findTag(dsl, req.getName()).ifPresentOrElse(tag -> {
-            dsl.deleteFrom(TAGS).where(TAGS.TAGNAME.eq(tag.getTagname())).execute();
-            auditor.log(AuditAction.DELETED_TAG, e.getAuthor().getIdLong());
-            Messenger.send(e.getChannel(), "Tag `" + tag.getTagname() + "` has been deleted.");
-        }, () -> Messenger.send(e.getChannel(), "Tag does not exist."));
+        dsl.deleteFrom(TAGS).where(TAGS.TAGNAME.eq(tag.getTagname())).execute();
+        auditor.log(AuditAction.DELETED_TAG, e.getAuthor().getIdLong());
+        Messenger.send(e.getChannel(), "Tag `" + tag.getTagname() + "` has been deleted.");
     }
 
     /**
