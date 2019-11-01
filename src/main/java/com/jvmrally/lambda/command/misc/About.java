994f7e9fb4388101e077830da0cde89a5a9e8031
Toby Larone <10854475+Toby-Larone@users.noreply.github.com>
2019-11-01 13:34:51 +0000

    [commands.about] Update github link

--- src/main/java/com/jvmrally/lambda/command/misc/About.java
+++ src/main/java/com/jvmrally/lambda/command/misc/About.java
@@ -18,7 +18,7 @@ public class About {
     public static void execute(MessageReceivedEvent e) {
         EmbedBuilder eb = new EmbedBuilder();
         eb.setTitle("**Lambda Bot**");
-        eb.addField("Source Code", "https://www.github.com/Toby-Larone/Lambda", false);
+        eb.addField("Source Code", "https://github.com/JVMRally/Lambda", false);
         eb.setColor(Color.WHITE);
         eb.setThumbnail(e.getJDA().getSelfUser().getEffectiveAvatarUrl());
         Messenger.send(e.getChannel(), eb.build());
