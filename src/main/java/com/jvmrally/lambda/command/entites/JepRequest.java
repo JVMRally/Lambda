797b9c26a774e139abac263760ba542cd6fa5164
Ladislav92 <milunovic.ladislav@gmail.com>
2019-11-09 21:43:32 +0100

    [commands] WIP: First working prototype for jep command. Output restrictions needed.

---
+++ src/main/java/com/jvmrally/lambda/command/entites/JepRequest.java
@@ -0,0 +1,44 @@
+package com.jvmrally.lambda.command.entites;
+
+import disparse.parser.reflection.Flag;
+import disparse.parser.reflection.ParsedEntity;
+
+@ParsedEntity
+public class JepRequest {
+
+    @Flag(longName = "jepId", description = "ID of the JEP")
+    private Integer jepId = -1;
+
+    @Flag(shortName = 's', longName = "search", description = "Searches JEP list by title")
+    private String searchParam = "";
+
+    @Flag(longName = "status", description = "Searches JEP list by status")
+    private String statusName = "";
+
+    @Flag(longName = "type", description = "Returns list of all JEPs for specified type")
+    private String type = "";
+
+    @Flag(longName = "release", description = "Returns list of all JEPs for specified target")
+    private Integer target = -1;
+
+    public Integer getJepId() {
+        return jepId;
+    }
+
+    public String getSearchParam() {
+        return searchParam;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Integer getTarget() {
+        return target;
+    }
+
+ }
---
+++ src/main/java/com/jvmrally/lambda/command/utility/JepCommand.java
@@ -0,0 +1,106 @@
+package com.jvmrally.lambda.command.utility;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import com.jvmrally.lambda.command.entites.JepRequest;
+import com.jvmrally.lambda.db.tables.pojos.Jeps;
+import com.jvmrally.lambda.injectable.JooqConn;
+import com.jvmrally.lambda.jdk.Jep;
+import com.jvmrally.lambda.utility.messaging.Messenger;
+
+import org.jooq.Condition;
+import org.jooq.impl.DSL;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import disparse.parser.reflection.CommandHandler;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import static com.jvmrally.lambda.db.Tables.JEPS;
+
+public class JepCommand {
+
+    private JepCommand() {
+    }
+
+    @CommandHandler(commandName = "jep", description = "Details information about the bot.")
+    public static void execute(final MessageReceivedEvent e, final JepRequest request) {
+     
+        // filter by id, return straight away if id present
+        if(request.getJepId() != -1){
+
+            var jep = JooqConn.getJooqContext()
+                                        .select()
+                                        .from(JEPS)
+                                        .where(JEPS.ID.equal(request.getJepId()))
+                                        .fetchInto(Jeps.class);
+
+            Jep found = jep.isEmpty() ? null : new Jep(jep.get(0));
+
+            sendFiltered(e.getChannel(), found);
+
+            return;
+        }
+
+        Condition result = DSL.trueCondition();
+
+        // add title search filter
+        if (!request.getSearchParam().isEmpty()){
+            result = result.and(JEPS.TITLE.like("%" + request.getSearchParam() + "%"));
+        }
+
+        // add status filter
+        if(!request.getStatusName().isEmpty()){
+            result = result.and(JEPS.JEP_STATUS.equal(request.getStatusName()));
+        }
+
+        // add release filter
+        if(request.getTarget() != -1){
+            result = result.and(JEPS.RELEASE.contains(Integer.toString(request.getTarget())));
+        }
+
+        // add type filter
+        if(!request.getType().isEmpty()){
+          result = result.and(JEPS.JEP_TYPE.equal(request.getType()));
+        }
+
+        final List<Jep> filtered = JooqConn.getJooqContext()
+                        .select()
+                        .from(JEPS)
+                        .where(result)
+                        .fetchInto(Jeps.class)
+                        .stream()
+                        .map(Jep::new)
+                        .collect(Collectors.toList());
+
+        if(filtered.isEmpty()){
+            sendFiltered(e.getChannel(), null);
+            return;
+        }
+
+        // If filtered > 3 send only 3 and for rest use TITLE : ID pairs
+
+        filtered.forEach( j -> sendFiltered(e.getChannel(), j));
+    }
+
+    private static void sendFiltered(final MessageChannel channel, final Jep jep){
+
+        if(jep == null){
+            Messenger.send(channel, "No JEP matching the criteria.");
+            return;
+        }
+
+        MessageEmbed message = new EmbedBuilder()
+                                .setTitle("**" + jep.getJepType() + " " + jep.getId() + ": " + jep.getTitle() + "**")
+                                .addField("Type", jep.getJepType().name(), true)
+                                .addField("Status", jep.getStatus().name(), true)
+                                .addBlankField(true)
+                                .addField("Java Release", jep.getRelease(), true)
+                                .addField("JDK Component", jep.getComponent(), true)
+                                .addBlankField(true)
+                                .addField("URL", jep.getUrl(), false)
+                                .build();
+
+        Messenger.send(channel, message);
+    }
+}
