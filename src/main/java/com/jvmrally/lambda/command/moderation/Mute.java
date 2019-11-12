994c213a419e6777dd768376c09e294d760b1aa6
Toby Larone <10854475+Toby-Larone@users.noreply.github.com>
2019-11-12 15:58:22 +0000

    [commands] Refactor Mute command

--- src/main/java/com/jvmrally/lambda/command/moderation/Mute.java
+++ src/main/java/com/jvmrally/lambda/command/moderation/Mute.java
@@ -18,51 +18,66 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
  */
 public class Mute {
 
+    private Auditor audit;
+    private DSLContext dsl;
+    private TimedReasonRequest req;
+    private MessageReceivedEvent e;
+
     private Mute() {
+    }
+
+    private Mute(Auditor audit, DSLContext dsl, TimedReasonRequest req, MessageReceivedEvent e) {
+        this.audit = audit;
+        this.dsl = dsl;
+        this.req = req;
+        this.e = e;
     }
 
     @CommandHandler(commandName = "mute",
             description = "Mute someone for the specified amount of time. Defaults to 1 hour.",
             roles = "admin")
-    public static void mute(Auditor audit, DSLContext dsl, TimedReasonRequest req,
+    public static void execute(Auditor audit, DSLContext dsl, TimedReasonRequest req,
             MessageReceivedEvent e) {
-        Util.getRole(e.getGuild(), "muted").ifPresentOrElse(
-                role -> muteMembers(audit, dsl, req, e, role),
+        Mute mute = new Mute(audit, dsl, req, e);
+        Util.getRole(e.getGuild(), "muted").ifPresentOrElse(mute::muteMembers,
                 () -> Messenger.send(e.getChannel(), "Role does not exist."));
     }
 
-
-
-    private static void muteMembers(Auditor audit, DSLContext dsl, TimedReasonRequest req,
-            MessageReceivedEvent e, Role role) {
-        Util.getMentionedMembers(e).ifPresentOrElse(
-                members -> filterMembers(audit, dsl, req, e, role, members),
+    private void muteMembers(Role role) {
+        Util.getMentionedMembers(e).ifPresentOrElse(members -> filterMembers(role, members),
                 () -> Messenger.send(e.getChannel(), "Must mention at least one user"));
     }
 
-    private static void filterMembers(Auditor audit, DSLContext dsl, TimedReasonRequest req,
-            MessageReceivedEvent e, Role role, List<Member> members) {
+    private void filterMembers(Role role, List<Member> members) {
         for (Member member : members) {
             if (!member.getRoles().contains(role)) {
-                muteMember(audit, dsl, req, e, role, member);
+                muteMember(role, member);
             }
         }
     }
 
-    private static void muteMember(Auditor audit, DSLContext dsl, TimedReasonRequest req,
-            MessageReceivedEvent e, Role role, Member member) {
+    private void muteMember(Role role, Member member) {
         e.getGuild().addRoleToMember(member, role).queue();
-        logMute(audit, dsl, req, e, member);
+        logMute(member);
     }
 
-    private static void logMute(Auditor audit, DSLContext dsl, TimedReasonRequest req,
-            MessageReceivedEvent e, Member member) {
-        saveMute(dsl, member, req);
+    /**
+     * Log the mute action
+     * 
+     * @param member
+     */
+    private void logMute(Member member) {
+        saveMute(member);
         audit.log(AuditAction.MUTED, e.getAuthor().getIdLong(), member.getIdLong(),
                 req.getReason());
     }
 
-    private static void saveMute(DSLContext dsl, Member member, TimedReasonRequest req) {
+    /**
+     * Save a mute record into the database
+     * 
+     * @param member
+     */
+    private void saveMute(Member member) {
         dsl.insertInto(MUTE).values(member.getIdLong(), req.getExpiry()).execute();
     }
 }
