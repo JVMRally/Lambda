6b68b83ce775ad40c5e7bcf62dd1b068eca327e6
Leslie <todo>
2020-03-10 23:28:51 +0100

    [modmail] #13 add correct channel creation and deletion

---
+++ src/main/java/com/jvmrally/lambda/modmail/CouldNotCreateChannelException.java
@@ -0,0 +1,10 @@
+package com.jvmrally.lambda.modmail;
+
+/**
+ * CouldNotCreateChannelException
+ */
+public class CouldNotCreateChannelException extends RuntimeException {
+    public CouldNotCreateChannelException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
