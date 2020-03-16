ca39b4bea397a833fc33bea86e203e4f8ad6e471
Leslie <todo>
2020-03-16 15:57:35 +0100

    [modmail] #13 ad cleartags

--- src/main/java/com/jvmrally/lambda/modmail/ModmailHandler.java
+++ src/main/java/com/jvmrally/lambda/modmail/ModmailHandler.java
@@ -42,6 +42,13 @@ public class ModmailHandler {
         this.jda = Objects.requireNonNull(jda);
     }
 
+    public void clearTags(TextChannel channel) {
+        var initialChannelName = channel.getName();
+        var newName = initialChannelName.replaceAll("[\\p{P}\\p{S}]", "");
+
+        channel.getManager().setName(newName).submit();
+    }
+
     public void tagChannel(TextChannel channel, String emote) {
         var initialChannelName = channel.getName();
         channel.getManager().setName(emote + initialChannelName).submit();
