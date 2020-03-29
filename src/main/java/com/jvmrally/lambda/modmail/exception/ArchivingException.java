7a9d3331bd72f282ea30652f7cb9fcaac41c4b1f
Leslie <todo>
2020-03-30 01:13:03 +0200

    [modmail] fix compilewarnings/sonarlint

--- src/main/java/com/jvmrally/lambda/modmail/exception/ArchivingException.java
+++ src/main/java/com/jvmrally/lambda/modmail/exception/ArchivingException.java
@@ -4,6 +4,8 @@ package com.jvmrally.lambda.modmail.exception;
  * ArchivingException
  */
 public class ArchivingException extends RuntimeException {
+    private static final long serialVersionUID = 2649393988071667847L;
+
     public ArchivingException(String message, Throwable cause) {
         super(message, cause);
     }
