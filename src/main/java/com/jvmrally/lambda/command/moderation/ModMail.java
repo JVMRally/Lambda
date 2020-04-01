d3c0f205a4f5ad8a0a5f5de56bd6edccd3f4099c
Leslie <todo>
2020-04-01 13:54:33 +0200

    [modmail] add command logging

--- src/main/java/com/jvmrally/lambda/command/moderation/ModMail.java
+++ src/main/java/com/jvmrally/lambda/command/moderation/ModMail.java
@@ -29,6 +29,7 @@ public class ModMail extends Command {
 
     @CommandHandler(commandName = "modmail.open", description = "If not already opened, opens a modmail channel for each given id.", roles = "admin")
     public static void open(List<String> userIds, MessageReceivedEvent e) {
+        logCommand(e);
         List<Guild> guilds = e.getJDA().getGuilds();
         for (var userId : userIds) {
             new ModmailChannelManagement(e.getJDA()).openNewChannel(getUser(userId, guilds));
@@ -37,11 +38,13 @@ public class ModMail extends Command {
 
     @CommandHandler(commandName = "modmail.close", description = "Closes the current modmail channel.")
     public static void close(MessageReceivedEvent e) {
+        logCommand(e);
         new ModmailChannelManagement(e.getJDA()).deleteChannel(e.getChannel(), e.getGuild());
     }
 
     @CommandHandler(commandName = "modmail.note", description = "Posts a note embed in the current channel.")
     public static void note(List<String> args, MessageReceivedEvent e) {
+        logCommand(e);
         var content = new StringBuilder();
         args.forEach(word -> content.append(content.length() == 0 ? "" : " ").append(word));
         new ModmailNoteHandler().postNote(e.getChannel(), e.getAuthor(), content.toString());
@@ -49,17 +52,20 @@ public class ModMail extends Command {
 
     @CommandHandler(commandName = "modmail.tag", description = "Prepends an emote to the channel name.")
     public static void tag(List<String> args, MessageReceivedEvent e) {
+        logCommand(e);
         var modmailTagHandler = new ModmailTagHandler();
         args.forEach(emote -> modmailTagHandler.tagChannel(e.getTextChannel(), emote));
     }
 
     @CommandHandler(commandName = "modmail.cleartags", description = "Clears all tags from the channel")
     public static void clearTags(MessageReceivedEvent e) {
+        logCommand(e);
         new ModmailTagHandler().clearTags(e.getTextChannel());
     }
 
     @CommandHandler(commandName = "modmail.archive", description = "Posts a log file to the reports-archive channel")
     public static void archive(List<String> args, MessageReceivedEvent e) {
+        logCommand(e);
         var additionalNote = new StringBuilder();
         args.forEach(word -> additionalNote.append(additionalNote.length() == 0 ? "" : " ").append(word));
         new ModmailChannelArchiver(e.getTextChannel(), e.getGuild()).archive(additionalNote.toString());
@@ -75,6 +81,11 @@ public class ModMail extends Command {
         }
 
         return foundUsers.get(0);
+    }
+
+    private static void logCommand(MessageReceivedEvent e) {
+        LOGGER.info(String.format("[%s] %s in channel %s: %s", "modmail", e.getAuthor().getAsTag(),
+                e.getChannel().getName(), e.getMessage().getContentRaw()));
     }
 
 }
