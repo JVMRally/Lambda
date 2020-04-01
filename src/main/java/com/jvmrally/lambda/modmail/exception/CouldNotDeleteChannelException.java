cd34037b76bd8791aa5a3e37f6b34b2264b34dfd
Leslie <todo>
2020-04-01 15:12:24 +0200

    [modmail] add exception when channel cannot be deleted

---
+++ src/main/java/com/jvmrally/lambda/modmail/exception/CouldNotDeleteChannelException.java
@@ -0,0 +1,12 @@
+package com.jvmrally.lambda.modmail.exception;
+
+/**
+ * CouldNotCreateChannelException
+ */
+public class CouldNotDeleteChannelException extends RuntimeException {
+    private static final long serialVersionUID = -4925638998536771992L;
+
+    public CouldNotDeleteChannelException(String message) {
+        super(message);
+    }
+}
