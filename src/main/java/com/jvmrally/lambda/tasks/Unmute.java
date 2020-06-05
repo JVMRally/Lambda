c542a78c1748fee40343c86ae16a20cb10f4b758
Jared Bosco <boscojared@gmail.com>
2020-06-05 16:38:40 -0400

    [listener] unmute now correctly respects guildIds when unmuting

--- src/main/java/com/jvmrally/lambda/tasks/Unmute.java
+++ src/main/java/com/jvmrally/lambda/tasks/Unmute.java
@@ -2,12 +2,15 @@ package com.jvmrally.lambda.tasks;
 
 import static com.jvmrally.lambda.db.tables.Mute.MUTE;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import javax.management.relation.RoleNotFoundException;
 import com.jvmrally.lambda.annotation.Task;
 import com.jvmrally.lambda.db.tables.pojos.Mute;
 import com.jvmrally.lambda.injectable.JooqConn;
 import com.jvmrally.lambda.utility.Util;
+import net.dv8tion.jda.api.entities.Guild;
 import org.jooq.DSLContext;
 import net.dv8tion.jda.api.JDA;
 
@@ -30,14 +33,22 @@ public class Unmute implements Runnable {
     @Override
     public void run() {
         DSLContext dsl = JooqConn.getJooqContext();
-        List<Mute> mutes = dsl.selectFrom(MUTE)
-                .where(MUTE.MUTE_EXPIRY.le(System.currentTimeMillis())).fetchInto(Mute.class);
-        var guild = jda.getGuilds().get(0);
-        for (Mute mute : mutes) {
+        Map<Long, Mute> mutes = dsl.selectFrom(MUTE)
+                .where(MUTE.MUTE_EXPIRY.le(System.currentTimeMillis())).fetchInto(Mute.class)
+                .stream()
+                .collect(Collectors.toMap(Mute::getGuildId, m -> m));
+
+        mutes.forEach((guildId, mute) -> {
+            Guild guild = jda.getGuildById(guildId);
+            if (guild == null) {
+                return;
+            }
             Util.getRole(guild, MUTED_ROLE).ifPresentOrElse(role -> {
                 guild.removeRoleFromMember(guild.getMemberById(mute.getUserid()), role).queue();
-                dsl.deleteFrom(MUTE).where(MUTE.USERID.eq(mute.getUserid())).execute();
+                dsl.deleteFrom(MUTE)
+                        .where(MUTE.USERID.eq(mute.getUserid()))
+                        .and(MUTE.GUILD_ID.eq(guildId)).execute();
             }, () -> new RoleNotFoundException(MUTED_ROLE));
-        }
+        });
     }
 }
